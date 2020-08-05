f1c4bb463f623599338d33b21e41249533cbac8e
Carlyn Carmel Orio <carlynorio@gmail.com>
2020-08-05 21:16:16 +0800

    update wp-config

--- wp/wp-config.php
+++ wp/wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'carlynorio_exam');
+define('DB_NAME', 'K---K-Hire-Ltd_db');
 
 /** MySQL database username */
 define('DB_USER', 'root');
